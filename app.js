261974c1658c3de0d84fc50cf037f55dfd4cb09b
Tom Gerrits <t.gerrits@christ.de>
2019-05-03 14:44:08 +0200

    add post route

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var mongoose = require('mongoose');
 var Book = require('./Book.model');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-var port = 8080;
+var port = 3000;
 var db = 'mongodb://localhost:27017/bookDB';
 mongoose.connect(db, {useNewUrlParser: true}, function(err) {
     if (err) {
@@ -16,21 +18,51 @@ mongoose.connect(db, {useNewUrlParser: true}, function(err) {
     }
 });
 
+// home root
 app.get('/', function(req, res) {
     res.send('happy to be here!');
 });
 
-app.get('/books', function(req, res) {
-    Book.find({})
-        .exec(function(err, books) {
+// get book by id
+app.get('/books/:id', function(req, res) {
+    Book.findById({_id: req.params.id})
+        .exec(function(err, book) {
             if (err) {
                 console.log('error has occured');
             } else {
-                res.status(200).json(books);
+                return res.status(200).json(book);
             }
         });
 });
 
+// get all the books
+app.get('/books', function(req, res) {
+    Book.find({})
+        .exec(function(err, books) {
+            if (err) {
+                return res.status(401);
+            } else {
+                return res.status(200).json(books);
+            }
+        });
+});
+
+app.post('/books', function(req, res) {
+    // oneway
+    var aBook = new Book();
+    aBook.title = req.body.title;
+    aBook.author = req.body.author;
+    aBook.category = req.body.category;
+    console.log(aBook);
+
+    aBook.save(function(err, savedBook) {
+        if (err) {
+            return res.send('error saving book');
+        } else {
+            return res.send(savedBook);
+        }
+    });
+});
 
 app.listen(port, function() {
     console.log('Server running on port '+port);
